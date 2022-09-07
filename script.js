aec2b80ec446925dad56df733e7c37d0080b9863
Snirb <86573025+Snirb@users.noreply.github.com>
2022-09-08 00:02:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function link() {
     let i = "https://web.whatsapp.com/send?phone=XXXX&text&type=phone_number&app_absent=0"
     let num = document.getElementById("num");
-    location.replace("https://web.whatsapp.com/send?phone=+972" + num.value + "&text&type=phone_number&app_absent=0");
+    if (screen.width < 599) {
+        location.replace("https://wa.me/+972" + num.value);
+    }
+    else {
+        location.replace("https://web.whatsapp.com/send?phone=+972" + num.value + "&text&type=phone_number&app_absent=0");
+    }
 }
